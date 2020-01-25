330dd59c03110ea0850af3962ad484d63218b84a
lambertco <blambertco@gmail.com>
2020-01-24 21:48:08 -0700

    mostly finished, still a bug on delete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,91 @@
-// ======================
-// VARIABLES
-// ======================
+//pull initial budgetItems/lastID from localStorage to set initial variables
+const budgetItems = JSON.parse(localStorage.getItem('budgetItem')) || [];
+let lastID = parseInt(localStorage.getItem('lastID')) || 0; 
 
-// 1st: pull initial budgetItems/lastID from localStorage to set initial variables
+//function to update localStorage with latest budgetItems and latest lastID
+const updateStorage = () => {
+    localStorage.setItem('budgetItems', JSON.stringify(budgetItems));
+    localStorage.setItem('lastID', lastID);
+}
 
+// function to render budgetItems on table; each item should be rendered in this format:
+const renderItems = items => {
+    if(!items) items = budgetItems;
+    const tbody =  $('#budgetItems tbody');
+    tbody.empty();
 
+    for(const {id, date, name, category, amount, notes} of items) {
+        const row = `<tr data-id=${id}><td>${date}</td><td>${name}</td><td>${category}</td><td>$${parseFloat(amount).toFixed(2)}</td><td>${notes}</td><td class="delete"><span>x</span></td></tr>`;
+       tbody.append(row);
+    }
 
-// ======================
-// FUNCTIONS
-// ======================
+    const total = items.reduce((accum, item) => 
+        accum + parseFloat(item.amount), 0);
+    $('#total').text(`$${total.toFixed(2)}`);
+}
 
-// 4th: function to update localStorage with latest budgetItems and latest lastID
+renderItems();
+//wire up click event on 'Enter New Budget Item' button to toggle display of form
+$('#toggleFormButton, #hideForm').on('click', function(event) {
+    // event.preventDefault();
+    const addItemForm = $('#addItemForm');
 
+    addItemForm.toggle('slow', () => {
+        $('#toggleFormButton').text(addItemForm.is(':visible') ? 'Hide form' : 'Enter new budget item');
+    });
+});
 
-// 5th: function to render budgetItems on table; each item should be rendered in this format:
-// <tr data-id="2"><td>Oct 14, 2019 5:08 PM</td><td>November Rent</td><td>Rent/Mortgage</td><td>1300</td><td>Fill out lease renewal form!</td><td class="delete"><span>x</span></td></tr>
-// also, update total amount spent on page (based on selected category):
-
-
-
-// ======================
-// MAIN PROCESS
-// ======================
-
-// 2nd: wire up click event on 'Enter New Budget Item' button to toggle display of form
-
-
-// 3rd: wire up click event on 'Add Budget Item' button, gather user input and add item to budgetItems array
+// wire up click event on 'Add Budget Item' button, gather user input and add item to budgetItems array
 // (each item's object should include: id / date / name / category / amount / notes)... then clear the form
 // fields and trigger localStorage update/budgetItems rerender functions, once created
 
+$('#addItem').on('click', function (event) {
+    event.preventDefault();
 
-// 6th: wire up change event on the category select menu, show filtered budgetItems based on selection
+    const newItem = {
+        id: ++lastID, // increment and store updated value in one step
+        date: moment().format('lll'),
+        name: $('#name').val().trim(),
+        category: $('#category').val(),
+        amount: $('#amount').val().trim(),
+        notes: $('#notes').val().trim()
+    }
 
+        if(!newItem.name || !newItem.category || !newItem.amount) {
+            //if failed validation
+          return alert('You must specify name, category, and amount for each budget item.');
+        }
 
-// 7th: wire up click event on the delete button of a given row; on click delete that budgetItem
+        budgetItems.push(newItem);
+        $('#addItemForm form')[0].reset();
+        updateStorage(); // update local storage
+        renderItems(); // re-render our budget items
 
+        localStorage.getItem(JSON.stringify(newItem));
+        console.log(newItem);
+  
+});
+// wire up change event on the category select menu, show filtered budgetItems based on selection
+ $('#categoryFilter').on('change', function() {
+     const category = $(this).val();
+     if(category) {
+     const filteredItems = budgetItems.filter(item => category === item.category);
+     renderItems(filteredItems)} else {
+         renderItems();
+     }
+ });
+
+// wire up click event on the delete button of a given row; on click delete that budgetItem
+$('#budgetItems').on('click', '.delete span', function() {
+    const id = parseInt($(this).parents('tr').data('id'));
+    const remainingItems = budgetItems.filter(item => 
+    item.id !== id
+    );   
+    budgetItems = remainingItems;
+    updateStorage();
+    renderItems();
+    $('#categoryFilter').val('');
+});
 
 
 
